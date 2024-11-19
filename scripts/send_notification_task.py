86ee5c915b9c8ac897cb2a857568cd805119a8a3
dsatoh20 <dsatoh@GQ2DPW2>
2024-11-19 13:15:28 +0900

    changed segments

--- scripts/send_notification_task.py
+++ scripts/send_notification_task.py
@@ -1,27 +1,28 @@
 import requests
 import json
 import os
-import django
 
-"""# Django環境を読み込む（必要に応じて設定）
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
-django.setup()"""
 API_KEY = os.environ.get('ONE_SIGNAL_API_KEY')
 
 def send_notification():
     url = "https://onesignal.com/api/v1/notifications"
     headers = {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": API_KEY,
+        "Authorization": f'Basic {API_KEY}',
     }
 
     payload = {
         "app_id": "91af5053-eb7e-4a58-b2e5-dc1328868ba5",
-        "included_segments": ["Subscribed Users"],
-        "headings": {"en": "Reminder",
-                     "ja": "今日のはみがきはもうお済みですか？"},
-        "contents": {"en": "Have you done your tasks today? Check them out!",
-                     "ja": "今日の分の色塗りをしましょう。"},
+        "target_channel": "push",
+        "included_segments": ["Total Subscriptions"],
+        "headings": {
+            "en": "Reminder",
+            "ja": "今日のはみがきはもうお済みですか？"
+        },
+        "contents": {
+            "en": "Have you done your tasks today? Check them out!",
+            "ja": "今日の分の色塗りをしましょう。"
+        },
     }
 
     try:
